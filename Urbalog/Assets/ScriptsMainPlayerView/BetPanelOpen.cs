8c9ebff8e3de85e97bab9ce92c56f34b8b2dbf59
Hugo <owydoo@gmail.com>
2020-06-14 19:34:12 +0200

    Added a bet panel that opens and closes

---
+++ Urbalog/Assets/ScriptsMainPlayerView/BetPanelOpen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetPanelOpen : MonoBehaviour
+{
+    public GameObject Panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenPanel()
+    {
+        if (Panel != null)
+        {
+            Panel.SetActive(true);
+        }
+    }
+
+    public void ClosePanel()
+    {
+        if (Panel != null)
+        {
+            Panel.SetActive(false);
+        }
+    }
+}
