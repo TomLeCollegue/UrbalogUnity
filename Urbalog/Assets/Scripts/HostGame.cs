28de5261bc51427854b825dededb8fa79ec55dea
Kubasik <tomkubasik.pro@gmail.com>
2020-06-16 11:04:10 +0200

    Main Menu scene ( not def )

---
+++ Urbalog/Assets/Scripts/HostGame.cs
@@ -0,0 +1,39 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class HostGame : MonoBehaviour
+{
+    [SerializeField]
+    private uint roomSize = 6;
+    public string roomName = "name";
+    private NetworkManager networkManager;
+
+    void Start()
+    {
+        networkManager = NetworkManager.singleton;
+
+    }
+    public void SetRoomName (string _name)
+    {
+        roomName = _name;
+    }
+    public void SetIP(string _ip)
+    {
+        networkManager.networkAddress = _ip;
+    }
+
+    public void CreateRoom()
+    {
+        if(roomName != "" && roomName != null)
+        {
+            Debug.Log("Creating Room: " + roomName + ", Max player : " + roomSize);
+            networkManager.StartHost();
+
+        }
+    }
+
+    public void JoinGame()
+    {
+        networkManager.StartClient();
+    }
+}
