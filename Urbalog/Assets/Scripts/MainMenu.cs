f284f198f9d9ad75253f81b1a6977ff0077274f9
laurent cutting <laurent.cutting@gmail.coml>
2020-06-16 14:27:13 +0200

    script added (not used)

---
+++ Urbalog/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+using Mirror;
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private NetworkManager networkManager = null;
+
+    [Header("UI")]
+    [SerializeField] private GameObject landingPagePanel = null;
+
+    public void HostLobby()
+    {
+        networkManager.StartHost();
+
+        landingPagePanel.SetActive(false);
+    }
+}
