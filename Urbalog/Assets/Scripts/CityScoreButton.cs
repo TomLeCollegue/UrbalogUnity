61c9c80bc92c9117eaf37bae8dfd4bd864c40008
Hugo <owydoo@gmail.com>
2020-06-17 16:49:21 +0200

    Created temporary city score panel
    So the city score can be implemented later without having to create a cityViewScene

---
+++ Urbalog/Assets/Scripts/CityScoreButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityScoreButton : MonoBehaviour
+{
+    public GameObject Panel;
+    public void OpenPanel()
+    {
+        if (Panel != null)
+        {
+            Panel.SetActive(true);
+        }
+    }
+
+    public void ClosePanel()
+    {
+        if (Panel != null)
+        {
+            Panel.SetActive(false);
+        }
+    }
+
+    public void TogglePanel()
+    {
+        bool _isActive = Panel.activeSelf;
+
+        Panel.SetActive(!_isActive);
+    }
+}
