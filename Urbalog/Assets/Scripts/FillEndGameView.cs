ff2efd6e35e3a7bde0c7be719eb53bf904652a27
Kubasik <tomkubasik.pro@gmail.com>
2020-06-22 14:31:37 +0200

    End game if enough buildings

--- Urbalog/Assets/Scripts/FillEndGameView.cs
+++ Urbalog/Assets/Scripts/FillEndGameView.cs
@@ -9,6 +9,8 @@ public class FillEndGameView : MonoBehaviour
     public TextMeshProUGUI AttractCity;
     public TextMeshProUGUI EnviCity;
     public TextMeshProUGUI FluidCity;
+    public TextMeshProUGUI LogisticCity;
+    public TextMeshProUGUI SentenceLogistic;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +32,31 @@ public class FillEndGameView : MonoBehaviour
         AttractCity.text = _game.cityAttractiveness.ToString();
         EnviCity.text = _game.cityEnvironment.ToString();
         FluidCity.text = _game.cityFluidity.ToString();
+        LogisticCity.text = _game.cityLogistic.ToString();
+        LogisticSentence();
+
+    }
+
+    private void LogisticSentence()
+    {
+        int LogisticScore = GameManager.singleton.game.cityLogistic;
+
+        if(LogisticScore <= -2)
+        {
+            SentenceLogistic.text = "D'un point de vue logistique, l'organisation de votre ville est à revoir.";
+        }
+        else if ((LogisticScore > -2) && (LogisticScore <= 0))
+        {
+            SentenceLogistic.text = "D'un point de vue logistique, l'organisation de votre ville est à revoir.";
+        }
+        else if ((LogisticScore > 0) && (LogisticScore <= 2))
+        {
+            SentenceLogistic.text = "D'un point de vue logistique, l'organisation de votre ville est à revoir.";
+        }
+        else
+        {
+            SentenceLogistic.text = "Le transport de marchandises se fait très facilement dans votre ville.";
+        }
 
     }
 }
