4572180ead4644fc981c4b20a6032e6973f0cfc6
Kubasik <tomkubasik.pro@gmail.com>
2020-06-23 15:28:57 +0200

    Design Map + camera Rotation

---
+++ Urbalog/Assets/BillBoard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBoard : MonoBehaviour
+{
+    private Transform cam;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + cam.forward);
+    }
+
+    private void Start()
+    {
+        cam = GameObject.Find("Main Camera").GetComponent<Transform>();
+    }
+}
---
+++ Urbalog/Assets/RenameBuilding.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RenameBuilding : MonoBehaviour
+{
+
+    public TextMeshProUGUI BuldingName;
+    public int test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        test = 0;
+        BuldingName.text = "Poste" + test;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        test += 1;
+        BuldingName.text = "Poste" + test;
+    }
+}
