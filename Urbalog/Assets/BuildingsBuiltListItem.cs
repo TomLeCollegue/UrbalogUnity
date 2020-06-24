9f204a3a58574863512483e02437ffd0de9a4aff
laurent cutting <laurent.cutting@gmail.coml>
2020-06-24 17:13:45 +0200

    buildings built list finished

---
+++ Urbalog/Assets/BuildingsBuiltListItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BuildingsBuiltListItem : MonoBehaviour
+{
+    public TextMeshProUGUI nameBuilding;
+    public TextMeshProUGUI descripLogBuilding;
+    public TextMeshProUGUI logScoreBuilding;
+
+    public void RenameBuilding(string _nameBuilding)
+    {
+       nameBuilding.text = _nameBuilding;
+    }
+    public void RenameDescripLog(string _descripLogBuilding)
+    {
+        descripLogBuilding.text = _descripLogBuilding;
+    }
+    public void RenameScoreLogstring(int _logScoreBuilding)
+    {
+        logScoreBuilding.text = _logScoreBuilding.ToString();
+    }
+}
