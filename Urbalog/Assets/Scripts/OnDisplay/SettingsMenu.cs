255f7a0f4d03666382407569d8ac7e7174ceb2fb
Hugo <owydoo@gmail.com>
2020-06-27 12:24:27 +0200

    Button on Lobby Rework that goes to a setting scene
    + back button on the settingsScene to go back to Lobby

---
+++ Urbalog/Assets/Scripts/OnDisplay/SettingsMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToSettingsScene()
+    {
+        SceneManager.LoadScene("SettingsScene");
+    }
+
+    public void GoBackToLobby()
+    {
+        SceneManager.LoadScene("LobbyRework");
+    }
+
+
+}
