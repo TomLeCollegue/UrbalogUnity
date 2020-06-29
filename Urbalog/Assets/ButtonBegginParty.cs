dd9200fdc85544e763739a1e763258aa465ebb41
Kubasik <tomkubasik.pro@gmail.com>
2020-06-29 15:02:39 +0200

    Button Begin hide if not server

---
+++ Urbalog/Assets/ButtonBegginParty.cs
@@ -0,0 +1,21 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBegginParty : NetworkBehaviour
+{
+    public GameObject button;
+    void Start()
+    {
+        Debug.Log("DisplayButton");
+
+        if (!isServer)
+        {
+            button.SetActive(false);
+            Debug.Log("NOTDisplayButton");
+        }
+    }
+
+    
+}
