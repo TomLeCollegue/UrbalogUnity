ac476f9f4ad71b47e74b571230017c565ad195a5
Hugo <owydoo@gmail.com>
2020-07-06 11:42:06 +0200

    The List populates with the buildings that are in the JSON File

---
+++ Urbalog/Assets/Scripts/Settings/BuildingSettingItem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingSettingItem : MonoBehaviour
+{
+    public Building building;
+    public TextMeshProUGUI buildingName;
+
+    
+
+    public Building ReturnBuildingFromJson()
+    {
+        Building[] _buildingList = JSONBuildings.loadBuildingsFromJSON("/buildings.json");
+
+        Building _building = _buildingList[PopulateBuildingsSettingsElement.buildingNumber];
+        PopulateBuildingsSettingsElement.buildingNumber++;
+        return _building;
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (building != null)
+        {
+            building = ReturnBuildingFromJson();
+            buildingName.text = building.name;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
---
+++ Urbalog/Assets/Scripts/Settings/PopulateBuildingsSettingsList.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulateBuildingsSettingsList : MonoBehaviour
+{
+    public Transform targetTransform;
+    public PopulateBuildingsSettingsElement itemDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Init()
+    {
+        Building[] _buildingList = JSONBuildings.loadBuildingsFromJSON("/buildings.json");
+        for (int i = 0; i < _buildingList.Length; i++)
+        {
+            PopulateBuildingsSettingsElement _elementDisplay = Instantiate(itemDisplay);
+            _elementDisplay.transform.SetParent(targetTransform, false);
+        }
+    }
+}
