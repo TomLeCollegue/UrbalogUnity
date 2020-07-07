71da5e0841f339693d5f6976dc0eacf6d5c94ee4
laurent cutting <laurent.cutting@gmail.com>
2020-07-07 14:22:16 +0200

    script button form view

---
+++ Urbalog/Assets/Scripts/OnDisplay/DisplayFormView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject form1;
+    public GameObject form2;
+    public GameObject form3;
+
+    public void DisplayForm1()
+    {
+        mainMenu.SetActive(false);
+        form1.SetActive(true);
+    }
+
+    public void DisplayForm2()
+    {
+        form1.SetActive(false);
+        form2.SetActive(true);
+    }
+
+    public void DisplayForm3()
+    {
+        form2.SetActive(false);
+        form3.SetActive(true);
+    }
+
+}
