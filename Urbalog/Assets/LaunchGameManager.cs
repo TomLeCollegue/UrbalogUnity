7f125d1cde3b1f835db2d013c5dc51c37b51f9ff
laurent cutting <laurent.cutting@gmail.com>
2020-07-08 10:13:35 +0200

    forms scripts started

--- Urbalog/Assets/LaunchGameManager.cs
+++ Urbalog/Assets/LaunchGameManager.cs
@@ -14,7 +14,6 @@ public class LaunchGameManager : MonoBehaviour
         GameObject.Find("NetworkManager").GetComponent<HostGame>().CreateRoom();
     }
 
-
     public void ChangeIP(string _ip)
     {
         GameObject.Find("NetworkManager").GetComponent<HostGame>().SetIP(_ip);
@@ -24,6 +23,51 @@ public class LaunchGameManager : MonoBehaviour
     {
         GameObject.Find("NetworkManager").GetComponent<HostGame>().SetPlayerName(_name);
     }
+
+    public void ChangeSurname(string _surname)
+    {
+        GameObject.Find("NetworkManager").GetComponent<HostGame>().SetPlayerSurname(_surname);
+    }
+
+    public void HandleGenderInputData(int _value)
+    {
+        if (_value == 0)
+        {
+            GameObject.Find("NetworkManager").GetComponent<HostGame>().SetGender("Homme");
+        }
+        if (_value == 1)
+        {
+            GameObject.Find("NetworkManager").GetComponent<HostGame>().SetGender("Femme");
+        }
+        if (_value == 2)
+        {
+            GameObject.Find("NetworkManager").GetComponent<HostGame>().SetGender("Ni l'un ni l'autre");
+        }
+    }
+
+    public void ChangeAge(string _age)
+    {
+        GameObject.Find("NetworkManager").GetComponent<HostGame>().SetAge(_age);
+    }
+
+    public void ChangeZipcode(string _zipcode)
+    {
+        GameObject.Find("NetworkManager").GetComponent<HostGame>().SetZipcode(_zipcode);
+    }
+
+    public void ChangeCompany(string _company)
+    {
+        GameObject.Find("NetworkManager").GetComponent<HostGame>().SetCompany(_company);
+    }
+    public void ChangeJobStatus(string _jobStatus)
+    {
+        GameObject.Find("NetworkManager").GetComponent<HostGame>().SetAge(_jobStatus);
+    }
+    public void ChangeField(string _field)
+    {
+        GameObject.Find("NetworkManager").GetComponent<HostGame>().SetField(_field);
+    }
+
 
     public void ChangePort(string _port)
     {
