92ee57cff7e0517746c52c75b67be34a8390461e
Kubasik <tomkubasik.pro@gmail.com>
2020-07-16 12:28:12 +0200

    End Game reworked, pop up logistic

---
+++ Urbalog/Assets/popUpLogistic.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class popUpLogistic : MonoBehaviour
+{
+    public GameObject PopUp;
+    public TextMeshProUGUI nameBuilding;
+    public TextMeshProUGUI descBuilding;
+    public TextMeshProUGUI LogisticScore;
+    public Image Logistic;
+
+    Color urbaGrey = new Color(0.58f, 0.63f, 0.71f);
+    Color urbaGreen = new Color(0.56f, 0.68f, 0.32f);
+    Color urbaRed = new Color(0.83f, 0.19f, 0.11f);
+
+    public void DisplayPopUp(Building building)
+    {
+        PopUp.SetActive(true);
+        FillInfoBuilding(building);
+    }
+
+    private void FillInfoBuilding(Building building)
+    {
+        nameBuilding.text = building.name;
+        descBuilding.text = building.logisticDescription;
+        LogisticScore.text = building.logisticScore.ToString();
+        ColorScore(building);
+    }
+
+    private void ColorScore(Building building)
+    {
+        if (building.logisticScore < 0)
+        {
+            Logistic.color = urbaRed;
+        }
+        else if ((building.logisticScore == 0))
+        {
+            Logistic.color = urbaGrey;
+        }
+        else
+        {
+            Logistic.color = urbaGreen;
+        }
+    }
+
+    public void DismissPopUP()
+    {
+        PopUp.SetActive(false);
+    }
+}
