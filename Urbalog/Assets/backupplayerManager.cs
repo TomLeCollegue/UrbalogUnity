3fad2e072358ab8e13cbba7f06de663ee6f0a318
Kubasik <tomkubasik.pro@gmail.com>
2020-07-24 11:31:18 +0200

    need to send ressource to the server from player

--- Urbalog/Assets/backupplayerManager.cs
+++ Urbalog/Assets/backupplayerManager.cs
@@ -18,13 +18,6 @@ public class backupplayerManager : NetworkBehaviour
 
     void BackupPlayers()
     {
-        for (int i = 0; i < GameManager.singleton.players.Count; i++)
-        {
-            Player player = GameManager.singleton.players[i];
-            GameManager.singleton.playersBackup.Add(new PlayerBackUp(player.role, player.namePlayer, player.playerFamilyName, player.gender, player.age, player.zipcode, player.company, player.jobStatus, player.field, player.nameRole, player.ID, player.nextTurn, player.scorePlayer, player.OldScore, player.num)); ;
-        }
-
-
         for (int i = 0; i < GameManager.singleton.playersBackup.Count; i++)
         {
             for (int j = 0; j < GameManager.singleton.players.Count; j++)
