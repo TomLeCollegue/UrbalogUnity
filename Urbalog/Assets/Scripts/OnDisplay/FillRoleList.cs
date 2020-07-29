eaa0be4e9c98e54af06435a74ff2d5f124a57a48
Hugo <owydoo@gmail.com>
2020-07-29 17:00:47 +0200

    There is a list role in role settings

---
+++ Urbalog/Assets/Scripts/OnDisplay/FillRoleList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FillRoleList : MonoBehaviour
+{
+
+    public GameObject RoleItem;
+    public Transform listHolder;
+    public List<GameObject> RoleItemInList = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    public void SpawnPlayerItem(Role _role)
+    {
+        GameObject Item = Instantiate(RoleItem, listHolder);
+        Item.GetComponent<NameRoleInList>().Rename(_role);
+
+        RoleItemInList.Add(Item);
+    }
+}
---
+++ Urbalog/Assets/Scripts/OnDisplay/NameRoleInList.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NameRoleInList : MonoBehaviour
+{
+    public TextMeshProUGUI roleName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Take all the informations of _role and print them in the prefab
+    /// </summary>
+    /// <param name="_role"></param>
+    public void Rename(Role _role)
+    {
+        roleName.text = _role.nameRole;
+    }
+}
